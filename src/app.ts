323beab5bbba0fe92f749547c61a0b1cc556458e
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-01-19 13:47:34 +0100

    Some cleaning

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const wrapper = document.querySelector(".movie-wrapper") as HTMLElement;
 async function getAllMovies() {
     const response = await fetch("https://swapi.dev/api/films/");
     const listOfMovies = await response.json();
-    return listOfMovies
+    return listOfMovies.results
 }
 
 function showAllMovies() {
@@ -13,33 +13,33 @@ function showAllMovies() {
     getAllMovies().then((listOfMovies)=>{
         loadingText.style.display = "none";
 
-        let movieObj = listOfMovies['results'];
-        movieObj.forEach((element: any) => {
+        let movieObj = listOfMovies;
+        movieObj.forEach((entries: any) => {
 
             const movieCard = document.createElement('div');
             movieCard.className = "movieCard";
             wrapper.append(movieCard);
 
-            let property: keyof typeof element;
-            for(property in element){
+            let property: keyof typeof entries;
+            for(property in entries){
                 if(property === "title"){     
                     const movieHeader = document.createElement('h3');
-                    movieHeader.innerHTML = `${element[property]}`;
+                    movieHeader.innerHTML = `${entries[property]}`;
                     movieCard.append(movieHeader);
                 }
                 else if(property === "director"){
                     const director = document.createElement('p');
-                    director.innerHTML = `Director: ${element[property]}`;
+                    director.innerHTML = `Director: ${entries[property]}`;
                     movieCard.append(director);
                 }
                 else if(property === "release_date"){
                     const releaseDate = document.createElement('p');
-                    releaseDate.innerHTML = `Release date: ${element[property]}`;
+                    releaseDate.innerHTML = `Release date: ${entries[property]}`;
                     movieCard.append(releaseDate);
                 }
                 else if(property === "opening_crawl"){
                     const openingCrawl = document.createElement('p');
-                    openingCrawl.innerHTML = `Opening Crawl: ${element[property]}`;
+                    openingCrawl.innerHTML = `Opening Crawl: ${entries[property]}`;
                     movieCard.append(openingCrawl);
                 }
             };
