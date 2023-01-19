0fc7bb4d81eac24ae45ddae54f6bbd8ad8c5f1cf
Emma Bjerknes <Bjerknes.Emma@gmail.com>
2023-01-19 14:40:57 +0100

    Sometimes a for-loop is nicer to look at

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ const wrapper = document.querySelector(".movie-wrapper") as HTMLElement;
 async function getAllMovies() {
     const response = await fetch("https://swapi.dev/api/films/");
     const listOfMovies = await response.json();
-    return listOfMovies.results
+    return listOfMovies
 }
 
 function showAllMovies() {
@@ -13,37 +13,23 @@ function showAllMovies() {
     getAllMovies().then((listOfMovies)=>{
         loadingText.style.display = "none";
 
-        let movieObj = listOfMovies;
-        movieObj.forEach((entries: any) => {
-
+        for (let i = 0; i< listOfMovies.results.length; i++){          
             const movieCard = document.createElement('div');
             movieCard.className = "movieCard";
-            wrapper.append(movieCard);
 
-            let property: keyof typeof entries;
-            for(property in entries){
-                if(property === "title"){     
-                    const movieHeader = document.createElement('h3');
-                    movieHeader.innerHTML = `${entries[property]}`;
-                    movieCard.append(movieHeader);
-                }
-                else if(property === "director"){
-                    const director = document.createElement('p');
-                    director.innerHTML = `Director: ${entries[property]}`;
-                    movieCard.append(director);
-                }
-                else if(property === "release_date"){
-                    const releaseDate = document.createElement('p');
-                    releaseDate.innerHTML = `Release date: ${entries[property]}`;
-                    movieCard.append(releaseDate);
-                }
-                else if(property === "opening_crawl"){
-                    const openingCrawl = document.createElement('p');
-                    openingCrawl.innerHTML = `Opening Crawl: ${entries[property]}`;
-                    movieCard.append(openingCrawl);
-                }
-            };
-        });
+            const movieHeader = document.createElement('h3');
+            const director = document.createElement('p');
+            const releaseDate = document.createElement('p');
+            const openingCrawl = document.createElement('p');
+
+            movieHeader.innerHTML = `${listOfMovies.results[i].title}`;
+            director.innerHTML = `Director: ${listOfMovies.results[i].director}`;
+            releaseDate.innerHTML = `Release date: ${listOfMovies.results[i].release_date}`;
+            openingCrawl.innerHTML = `Opening Crawl: ${listOfMovies.results[i].opening_crawl}`;
+
+            wrapper.append(movieCard);
+            movieCard.append(movieHeader, director, releaseDate, openingCrawl);
+        }
     });
 };
 
